348fcdc0e11c5e1164b1aa44d3857c53d9fa629f
mstanton887@outlook.com <mstanton887@outlook.com>
2022-02-07 01:27:25 -0700

    day one practice

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace homeworkpractice_2._0 
+{
+    internal class Program
+    {
+        public class Factory
+        {
+            public int employeeNumber;
+            public string employeeName;
+            public string product;
+            public int productCount{get; }
+
+            public Factory(string employeeName, int employeeNumber, string product)
+            {
+                this.employeeName = employeeName;
+
+                this.employeeNumber = employeeNumber;
+
+                this.product = product;              
+            }
+        }
+        public class Employee : Factory 
+        {
+            public Employee(string employeeName, int employeeNumber, string product) : base(employeeName, employeeNumber, product)
+            {
+                employeeName = "Evan";
+                employeeNumber = 172646;
+                product = "Toy train";
+            }
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
