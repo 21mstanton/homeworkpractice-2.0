1846d496139a033487db0c6a580d2f26bd083c27
21mstanton <mstanton887@gmail.com>
2022-02-09 04:30:59 -0700

    interface practice

--- Program.cs
+++ Program.cs
@@ -7,8 +7,11 @@ namespace homeworkpractice_2._0
         public class Factory
         {
             public int employeeNumber;
+
             public string employeeName;
+
             public string product;
+
             public int productCount{get; }
 
             public Factory(string employeeName, int employeeNumber, string product)
@@ -20,15 +23,26 @@ namespace homeworkpractice_2._0
                 this.product = product;              
             }
         }
-        public class Employee : Factory 
+
+        public interface IProduct 
+        {
+            public string productName{get; set;}
+
+            public string productDescription{get; set;}
+        }
+
+        public class Employee : Factory : IProduct
         {
             public Employee(string employeeName, int employeeNumber, string product) : base(employeeName, employeeNumber, product)
             {
                 employeeName = "Evan";
                 employeeNumber = 172646;
                 product = "Toy train";
+                this.productName = "Toy train";
+                this.productDescription = "it needs to run on sold tracks";
             }
         }
+
         static void Main(string[] args)
         {
 
